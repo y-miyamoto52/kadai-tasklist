24e0f004c34bbdc83f302f6d5180e9cecb5ed0ba
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-10 13:03:05 +0000

    完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,58 @@
 class ApplicationController < ActionController::Base
   
   def index
+    @tasks = Task.all
   end
-
+#taskモデルの一覧を取得
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+     @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に作成されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が作成されませんでした'
+      render :new
+    end
   end
 
   def edit
+     @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+
+      if @task.update(task_params)
+        flash[:success] = 'Task は正常に更新されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'Task は更新されませんでした'
+        render :edit
+      end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
 end
+
+
+private
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
