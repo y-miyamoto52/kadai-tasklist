d6b4f701e6bef82c8f1f2c87a3a82950867a8577
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-10 11:44:13 +0000

    controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
