f4baf5fc870667335b5da7857e39fb878b9bce83
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-15 02:02:56 +0000

    confirmation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = Task.order(id: :desc).page(params[:page]).per(10)
   end
 
   def show
