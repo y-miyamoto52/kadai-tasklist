99c32b1d93ad2287d3be87369fe572bb0a8aa6ff
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-27 06:03:18 +0000

    create user action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録完了"
+      redirect_to @tasks
+    else
+      flash.now[:danger] = "登録に失敗しました"
+      render :new
+    end
+  end
+end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
   
   resources :tasks
