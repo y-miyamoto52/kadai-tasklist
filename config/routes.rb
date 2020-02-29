8db5aeed14d0d7d71873d9d982d5eaabad9ccbdd
yasuyuki.miyamoto <y.miyamoto52@gmail.com>
2020-02-29 12:57:50 +0000

    check2

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:index, :new, :create]
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
